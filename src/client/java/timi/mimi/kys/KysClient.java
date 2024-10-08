7d5a4a7a033929aec33b9d668b12a762318d450c
TymekBrunka <tymkopolo@wp.pl>
2024-10-08 17:01:41 +0200

    init

---
+++ src/client/java/timi/mimi/kys/KysClient.java
@@ -0,0 +1,10 @@
+package timi.mimi.kys;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class KysClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
