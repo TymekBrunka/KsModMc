ce4170c0d0b29e30cea61fc9bb06933f037dbf06
TymekBrunka <tymkopolo@wp.pl>
2024-10-09 12:40:43 +0200

    added loading world console log

--- src/main/java/timi/mimi/kys/mixin/ExampleMixin.java
+++ src/main/java/timi/mimi/kys/mixin/ExampleMixin.java
@@ -6,10 +6,16 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Mixin(MinecraftServer.class)
 public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "loadWorld")
-	private void init(CallbackInfo info) {
-		// This code is injected into the start of MinecraftServer.loadWorld()V
-	}
+    @Inject(at = @At("HEAD"), method = "loadWorld")
+    private void init(CallbackInfo info) {
+        // This code is injected into the start of MinecraftServer.loadWorld()V
+        String MOD_ID = "kys";
+        Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+        LOGGER.info("KYS loaded to world");
+    }
 }
